4aecc7db71671439f57612a060ef4fa8de187ce5
Jacob Perez <rrattusss@gmail.com>
2025-10-17 16:41:43 -0700

    demos and recording

--- Assets/Scripts/Testing/DemoRevolve.cs
+++ Assets/Scripts/Testing/DemoRevolve.cs
@@ -4,6 +4,7 @@ public class DemoRevolve : MonoBehaviour
 {
     [SerializeField] Transform revolver;
     [SerializeField] float radius = 0.5f;
+    [SerializeField] float speed = 1f;
 
     void Start()
     {
@@ -13,6 +14,9 @@ public class DemoRevolve : MonoBehaviour
     void Update()
     {
         Vector3 offset = Vector3.zero;
+
+        offset.x = Mathf.Sin(Time.time * speed) * radius;
+        offset.z = Mathf.Cos(Time.time * speed) * radius;
         
         revolver.transform.position = transform.position + offset;
     }
